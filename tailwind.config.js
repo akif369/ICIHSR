27de6ea6b6fffad27a0a2bb29b5296ca3b29a7f7
akif <akeef.anwar.10@gmail.com>
2023-12-14 23:53:10 +0530

    Design change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily:{
+          robo:['Silkscreen',"sans-serif"]
+        }
+      },
     },
     plugins: [],
   }
